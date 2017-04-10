6ac121c9b39f339a66ce0ded09ec27c751395396
richard-dev <richardl890@gmail.com>
2017-04-10 08:34:50 -0400

    Initial commit for UI Builder.

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -17,21 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Label
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        self.label = label
-        
-        // Button
-        let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
+    
     }
 
     func incrementCount() {
